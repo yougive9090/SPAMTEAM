95bb01a89295770d8e9d114c76cbe7a3224759ee
hoonkamsa <hoon.kamsa@gmail.com>
2026-05-21 20:13:32 +0900

    7

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Menu from './js/menu';
+import HunMenu from './js/hun_menu';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function Home() {
   return (
     <>
       <Menu />
+      <HunMenu />
     </>
   );
 }
---
+++ project/src/js/hun_menu.js
@@ -0,0 +1,13 @@
+import './hun_menu.css';
+import { Link } from 'react-router-dom';
+
+function HunMenu() {
+
+    return (
+        <div className="sjhun">
+            게시판
+        </div>
+
+    );
+}
+export default HunMenu;
